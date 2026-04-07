a07203c2279bea05568e74308d70dfc5fe963e93
ritakansal <ritakansal@users.noreply.github.com>
2026-04-07 11:33:47 +1000

    Add files via upload

---
+++ FindArea.py
@@ -0,0 +1,9 @@
+def calculate_area(length, width):
+    """Calculates the area of a rectangle."""
+    area = length * width
+    return area
+
+# Usage
+result = calculate_area(5, 10) # Call a function
+result2 = calculate_area(4, 15) # Call a function
+print(f"The area is: {result}")
