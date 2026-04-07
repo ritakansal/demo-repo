0c2c7cf324efea528db6a0524a5a64159ee9e1bd
ritakansal <ritakansal@users.noreply.github.com>
2026-04-07 11:51:54 +1000

    Update FindArea.py

--- FindArea.py
+++ FindArea.py
@@ -7,5 +7,6 @@ def calculate_area(length, width):
 result = calculate_area(5, 10) # Call a function
 result2 = calculate_area(4, 15) # Call a function
 result3 = calculate_area(5, 20) # Call a function
+result4 = calculate_area(5, 20) # Call a function
 print(f"The area is: {result}")
 print(f"The area is: {result3}"
